febb91bbf095f2d127b73a1bb2f6266fbef249f7
sumanRam1994 <suman.ram.infosys@gmail.com>
2020-05-31 19:32:45 +0530

    angular-final-first-commit

--- app/property/properties.js
+++
@@ -1,2 +0,0 @@
-var news_api= "http://newsapi.org/v2/top-headlines?";
-var news_api_key= "0651db9c823c4a5992fe58e6bbe1b988";
---
+++ src/app/model/content.ts
@@ -0,0 +1,13 @@
+import { Category } from './category'
+
+export class Content {    
+    id: number;
+    content: String;
+    description: String;
+    categories_id: number;
+    is_approved: String;
+    picture_location: String;
+    category: Category;
+    createdAt:Date;
+    updatedAt:Date;    
+}
