d4280bbfae0581e510904a7ca81012321e79c7b1
Saptarshi Bhattacharjee <saptarshi221292@gmail.com>
2020-07-12 12:40:34 +0530

    Front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { SportsComponent } from './components/sports/sports.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeaturedComponent,
+    SportsComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
